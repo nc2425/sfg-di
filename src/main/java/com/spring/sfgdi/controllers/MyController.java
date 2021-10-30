df9e3ba025e4e7f7e1cd20e62b2e4de2616ad4c9
Nelson Costa <ncosta2408@gmail.com>
2021-10-30 21:23:37 +0100

    Primary Beans

--- src/main/java/com/spring/sfgdi/controllers/MyController.java
+++ src/main/java/com/spring/sfgdi/controllers/MyController.java
@@ -1,14 +1,19 @@
 package com.spring.sfgdi.controllers;
 
+import com.spring.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello() {
 
-        System.out.println("Hello World!!!");
-
-        return "Hi Folks!";
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/com/spring/sfgdi/services/PrimaryGreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package com.spring.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the PRIMARY Bean";
+    }
+}
