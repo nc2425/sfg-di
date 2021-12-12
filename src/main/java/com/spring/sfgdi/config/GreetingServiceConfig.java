177c97d95188e37c7960a8e72e1dc9b5a4526e94
Nelson Costa <ncosta2408@gmail.com>
2021-12-12 20:36:54 +0000

    Spring Boot application.properties

--- src/main/java/com/spring/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/spring/sfgdi/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import org.example.pets.PetServiceFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
